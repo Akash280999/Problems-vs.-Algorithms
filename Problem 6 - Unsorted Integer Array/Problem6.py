eb7b680ff5c176dcfa0758577770d2c018da77a6
Akash Kumar <akashkrcse1@gmail.com>
2020-07-11 19:43:22 +0530

    Added more test cases

--- Problem 6 - Unsorted Integer Array/Problem6.py
+++ Problem 6 - Unsorted Integer Array/Problem6.py
@@ -6,6 +6,10 @@ def get_min_max(array):
        ints(list): list of integers containing one or more integers
        :param array:
     """
+    if len(array) <= 1:     # if empty array passed or having only one element
+        print([])
+        return None
+
     min_value = array[0]
     max_value = array[len(array) - 1]
 
@@ -27,5 +31,8 @@ l = [i for i in range(0, 10)]  # a list containing 0 - 9
 for i in range(4):
     random.shuffle(l)
     print(l)
-    print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
-    print("***************")
+    print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")     # should print Pass for 4 test cases as it in loop
+    print()
+
+print ("Pass\n" if (None == get_min_max([])) else "Fail")     # should print Pass
+print ("Pass\n" if (None == get_min_max([0])) else "Fail")     # should print Pass
