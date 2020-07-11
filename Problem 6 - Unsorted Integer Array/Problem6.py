1118bb0963d1260e9f575961c1b4dfbc323f1299
Akash Kumar <akashkrcse1@gmail.com>
2020-07-11 13:37:58 +0530

    Complete

--- Problem 6 - Unsorted Integer Array/Problem6.py
+++ Problem 6 - Unsorted Integer Array/Problem6.py
@@ -1,16 +1,31 @@
-def get_min_max(ints):
+def get_min_max(array):
     """
     Return a tuple(min, max) out of list of unsorted integers.
 
     Args:
        ints(list): list of integers containing one or more integers
+       :param array:
     """
-    pass
+    min_value = array[0]
+    max_value = array[len(array) - 1]
+
+    for number in array:
+        if array[number] > max_value:
+            max_value = array[number]
+        else:
+            if array[number] < min_value:
+                min_value = array[number]
+
+    return min_value, max_value
+
 
 ## Example Test Case of Ten Integers
 import random
 
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
-random.shuffle(l)
 
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+for i in range(4):
+    random.shuffle(l)
+    print(l)
+    print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+    print("***************")
